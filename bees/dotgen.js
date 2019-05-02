03bc5654f2a5cad9938b344f3bf63e7ae6d120d7
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-01 22:46:27 -0400

    bees & bombs

---
+++ bees/dotgen.js
@@ -0,0 +1,7 @@
+for(let i=0;i<12;i++){
+    let dot = document.createElement('div');
+    dot.setAttribute('class','dot');
+    document.getElementById('ring').appendChild(dot);
+}
+
+console.dir(ring);
